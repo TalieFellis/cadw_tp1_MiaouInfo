55d3d7adfd7fb6b1eca870d7324bbeee8c2dda67
LaylaTamblini <layla.tamb@gmail.com>
2024-08-30 22:25:11 -0400

    Ajout lien accueil barre navigation

--- resources/views/components/barre-navigation.blade.php
+++ resources/views/components/barre-navigation.blade.php
@@ -1,6 +1,8 @@
 <header class="bg-gray-900 text-white py-4">
     <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-2xl font-bold">MiaouInfo</h1>
+        <a href="{{ route('index') }}" class="text-2xl font-bold">
+            MiaouInfo
+        </a>
         <nav>
             <a href="{{ route('index') }}" class="mx-2 hover:text-gray-300">Accueil</a>
             <a href="{{ route('faits.list') }}" class="mx-2 hover:text-gray-300">Tous les faits</a>
